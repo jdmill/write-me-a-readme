81c4e04d25572c1579c71e0e93d796e10c12611a
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-04-15 16:08:43 -0400

    fixed the inquirer questions and got the Licenses to display properly on the README

--- index.js
+++ index.js
@@ -2,81 +2,83 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile)
+const genFile = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = {
   title: "What is this README's title?",
   desc: "What is this projects description?",
+  installBoo: "Would you like to add an install instruction?",
+  installNum: "How many instructions would you like to include",
   install: "What are the installation instructions?",
   usage: "What are the usage instructions?",
-  license: "What License was used?",
+  licenseBoo: "Include a license?",
+  license: "What license should be included?",
   contribution: "What are the contribution guidelines?",
   tests: "What tests were used for this project?",
   userName: "What is your github user name?",
   email: "What is your email address?",
 };
 
-inquirer.prompt([
-  {
-    type: "input",
-    name: "title",
-    message: questions.title,
-  },
-  {
-    type: "input",
-    name: "desc",
-    message: questions.desc,
-  },
-  {
-    type: "input",
-    name: "install",
-    message: questions.install,
-  },
-  {
-    type: "input",
-    name: "usage",
-    message: questions.usage,
-  },
-  {
-    type: "list",
-    name: "licenseType",
-    message: "Which license would you like to use?",
-    choices: ["MIT", "Apache 2.0", "GNU General Public License v3.0", "None"],
-  },
-  {
-    type: "input",
-    name: "contribution",
-    message: questions.contribution,
-  },
-  {
-    type: "input",
-    name: "tests",
-    message: questions.tests,
-  },
-  {
-    type: "input",
-    name: "userName",
-    message: questions.userName,
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is your email address?",
-  },
-])
-  .then((answers) => {
-    const readmePageContent = generateREADME(answers);
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: questions.title,
+    },
+    {
+      type: "input",
+      name: "desc",
+      message: questions.desc,
+    },
+    {
+      type: "input",
+      name: "install",
+      message: questions.install,
 
-    fs.writeToFile('README.md', readmePageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created README.md!!!!')
-    );
-  });
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: questions.usage,
+    },
+    {
+      type: "list",
+      name: "license",
+      message: questions.license,
+      choices: ["MIT", "Apache 2.0", "GNU GPL v3", "IBM", "None"],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: questions.contribution,
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: questions.tests,
+    },
+    {
+      type: "input",
+      name: "userName",
+      message: questions.userName,
+    },
+    {
+      type: "input",
+      name: "email",
+      message: questions.email,
+    },
+  ]);
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
+const init = () => {
+  promptUser()
+    .then((answers) => writeFileAsync('README.md', genFile(answers)))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
+};
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,75 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  //return Github license
+  if (license === "None") {
+    return "";
+  } else if (license === "Apache 2.0") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (license === "GNU GPL v3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "IBM") {
+    return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`;
+  } 
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'None') {
+    return '';
+  }
+  return ` * [License](#license)`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
 ## Description
-${data.desc}
+* ${data.desc}
 
-##Table of Contents
+## Table of Contents
 
--[Installation](#installation)
--[Usage](#usage)
--[Contribution](#contribution)
--[Tests](#tests)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [Questions](#questions)
+${renderLicenseLink(data.license)}
+
+
+## Installation
+* ${data.install}
+
+## Usage
+* ${data.usage}
+
+## Contribution
+* ${data.contribution}
+
+## Tests
+* ${data.tests}
+
+## Questions
+
+Please reach out with any questions you may have
+
+* Github: [Github:${data.userName}](https://www.github.com/${data.userName})
+
+* Email: [${data.email}](mailto:${data.email})
 
 `;
 }
